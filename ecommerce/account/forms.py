4c145814bf988271fc1d32ae438cb5fd8a6515b7
Mykyta <evolveddrago@gmail.com>
2024-03-11 18:01:55 +0100

    Add user managment

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -1,8 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from django.contrib.auth.models import User
+from django.forms.widgets import PasswordInput, TextInput
 
 
+
+
+# Registration form
 class CreateUserForm(UserCreationForm):
 
     class Meta:
@@ -30,3 +34,28 @@ class CreateUserForm(UserCreationForm):
 
 
         return email
+
+
+
+#Login Form
+class LoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
+
+
+#Update form
+
+class UpdateUserForm(forms.ModelForm):
+    password = None
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].required = True
+    
+    class Meta:
+        model = User
+
+        fields = ['username', 'email']
+        exclude = ['password1', 'password2']
